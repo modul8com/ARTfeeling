fcbebfa61ab9208ce3935e902df8cfd0eb544e76
Michael Bruyninckx <michael@modul8.com>
2018-09-08 19:26:27 +0200

    - upgraded cli and project - removed footer and header components

--- ARTfeeling.App/src/app/app.module.ts
+++ ARTfeeling.App/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PortfolioComponent,
-    HeaderComponent,
-    FooterComponent
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
